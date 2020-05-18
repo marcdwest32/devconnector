791b94dc653f83bc720653fb923f6e99826a61fa
Your marcdwest32 <marcdwest@gmail.com>
2020-05-17 21:59:15 -0500

    jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -58,8 +60,22 @@ router.post(
 
       await user.save();
 
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
       // Return jsonwebtoken
-      res.send('User Registered');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        },
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');

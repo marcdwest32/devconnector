5aa74c6ca094984b284a92adec9b3ce0dab45c4f
Marc West <marcdwest@gmail.com>
2020-10-08 11:27:17 -0500

    Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const auth = require('../../middleware/auth')
+const mongoose = require('mongoose')
 
 const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
@@ -38,5 +39,67 @@ router.post(
     }
   },
 )
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @ access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 })
+    res.json(posts)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @ access Private
+router.get('/:id', auth, async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ msg: 'Post not found' })
+  }
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    res.json(post)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by ID
+// @ access Private
+router.delete('/:id', auth, async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ msg: 'Post not found' })
+  }
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    await post.remove()
+
+    res.json({ msg: 'Post removed' })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server Error')
+  }
+})
 
 module.exports = router

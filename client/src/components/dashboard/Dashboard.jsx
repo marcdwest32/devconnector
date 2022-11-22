67a700b3654d0dd73ed13f9a33abfe90f47df354
Your marcdwest32 <marcdwest@gmail.com>
2022-11-22 17:52:14 -0600

    Dashboard - profile undefined

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,13 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import Spinner from '../layout/Spinner'
 import { getCurrentProfile } from '../../actions/profile'
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile }) => {
   useEffect(() => {
     getCurrentProfile()
   }, [getCurrentProfile])
-  return <div className='container'>Dashboard</div>
+
+  return (
+    <div className='container'>
+      <h1 className='large text-primary'>Dashboard</h1>{' '}
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome {user && user.name}{' '}
+      </p>{' '}
+    </div>
+  )
+  //   return loading && profile === null ? (
+  //     <Spinner />
+  //   ) : (
+  //     <div className='container'>
+  //       <h1 className='large text-primary'>Dashboard</h1>
+  //       <p className='lead'>
+  //         <i className='fas fa-user'></i> Welcome {user && user.name}
+  //       </p>
+  //     </div>
+  //   )
 }
 
 Dashboard.propTypes = {

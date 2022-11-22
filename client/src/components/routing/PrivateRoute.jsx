e5ab0d67c92b73a88679561e138a6ffa60f5cd21
Your marcdwest32 <marcdwest@gmail.com>
2022-11-22 13:52:52 -0600

    fix PrivateRoute

--- client/src/components/routing/PrivateRoute.jsx
+++ client/src/components/routing/PrivateRoute.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Route, redirect } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 const PrivateRoute = ({
   component: Component,
   auth: { isAuthenticated, loading },
-  ...rest
-}) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      !isAuthenticated && !loading ? (
-        redirect('/login')
-      ) : (
-        <Component {...props} />
-      )
-    }
-  />
-)
+}) => {
+  if (isAuthenticated) return <Component />
+
+  return <Navigate to='/login' />
+}
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired,

7f2d45a00f6d3f8b8f4729bf2835200d2f938cf6
Your marcdwest32 <marcdwest@gmail.com>
2022-12-02 14:57:26 -0600

    fix Go Back

--- client/src/components/profile-forms/AddExperience.jsx
+++ client/src/components/profile-forms/AddExperience.jsx
@@ -111,9 +111,9 @@ const AddExperience = ({ addExperience }) => {
           ></textarea>
         </div>
         <input type='submit' className='btn btn-primary my-1' />
-        <a className='btn btn-light my-1' href='dashboard.html'>
+        <Link className='btn btn-light my-1' to='/dashboard'>
           Go Back
-        </a>
+        </Link>
       </form>
     </div>
   )

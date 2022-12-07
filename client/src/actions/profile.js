a5abfdb5c8b2309f32dbc15a32d71076077252f6
Your marcdwest32 <marcdwest@gmail.com>
2022-12-07 11:34:12 -0600

    fix getGithubRepos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -63,7 +63,7 @@ export const getProfiles = () => async (dispatch) => {
 //Get Github repos
 export const getGithubRepos = (username) => async (dispatch) => {
   try {
-    const res = await axios.get(`api/profile/github/${username}`)
+    const res = await axios.get(`/api/profile/github/${username}`)
     dispatch({
       type: GET_REPOS,
       payload: res.data,

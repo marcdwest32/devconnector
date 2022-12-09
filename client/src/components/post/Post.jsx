be1c6e35671c1d1307bef6cd6116c97da57b410a
Your marcdwest32 <marcdwest@gmail.com>
2022-12-09 10:02:02 -0600

    delete a comment

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
 import CommentForm from '../post/CommentForm'
+import CommentItem from '../post/CommentItem'
 import { getPost } from '../../actions/post'
 
 const Post = ({ getPost, post: { post, loading } }) => {
@@ -22,6 +23,11 @@ const Post = ({ getPost, post: { post, loading } }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </div>
   )
 }

649791d09b03a695ae695dfbb01521d98e77024e
Your marcdwest32 <marcdwest@gmail.com>
2022-12-09 22:04:54 -0600

    Not Found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './components/dashboard/Dashboard'
-import CreateProfile from './components/profile-forms/CreateProfile'
-import EditProfile from './components/profile-forms/EditProfile'
-import AddEducation from './components/profile-forms/AddEducation'
-import AddExperience from './components/profile-forms/AddExperience'
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
-import Alert from './components/layout/Alert'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Profiles from './components/profiles/Profiles'
-import Profile from './components/profile/Profile'
-import Post from './components/post/Post'
-import Posts from './components/posts/Posts'
-import PrivateRoute from './components/routing/PrivateRoute'
 // Redux
 import { Provider } from 'react-redux'
-import store from './store'
 import { loadUser } from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
+import store from './store'
+// Components
+import AddEducation from './components/profile-forms/AddEducation'
+import AddExperience from './components/profile-forms/AddExperience'
+import Alert from './components/layout/Alert'
+import CreateProfile from './components/profile-forms/CreateProfile'
+import Dashboard from './components/dashboard/Dashboard'
+import EditProfile from './components/profile-forms/EditProfile'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Navbar from './components/layout/Navbar'
+import NotFound from './components/layout/NotFound'
+import Post from './components/post/Post'
+import Posts from './components/posts/Posts'
+import Profile from './components/profile/Profile'
+import Profiles from './components/profiles/Profiles'
+import PrivateRoute from './components/routing/PrivateRoute'
+import Register from './components/auth/Register'
 import './App.css'
 
 if (localStorage.token) {
@@ -64,6 +66,7 @@ const App = () => {
           />
           <Route path='posts' element={<PrivateRoute component={Posts} />} />
           <Route path='posts/:id' element={<PrivateRoute component={Post} />} />
+          <Route path='/*' element={<NotFound />} />
         </Routes>
       </Router>
     </Provider>
